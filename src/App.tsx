3338f1afb06b0cd316b91e434b3bef78872999cc
nartvt <tooyoutooyou@gmail.com>
2021-09-07 11:17:53 +0700

    implement pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Login from "./pages/Login";
+import Nav from "./components/Nav";
+import {BrowserRouter, Route} from "react-router-dom";
+import Register from "./pages/Register";
+import Home from "./pages/Home";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <Nav/>
+                <main className="form-signin">
+                    <Route path="/" exact component={Home}/>
+                    <Route path="/login" component={Login}/>
+                    <Route path="/register" component={Register}/>
+                </main>
+            </BrowserRouter>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/pages/Login.tsx
@@ -0,0 +1,39 @@
+import React, {SyntheticEvent, useState} from 'react';
+import {Redirect} from "react-router-dom";
+
+const Login = () => {
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [redirect, setRedirect] = useState(false);
+
+    const submitEvent = async (e: SyntheticEvent) => {
+        e.preventDefault();
+       await fetch("http://localhost:9000/api/login", {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'},
+           credentials: 'include',
+            body: JSON.stringify({
+                email,
+                password
+            })
+        });
+       setRedirect(true)
+    }
+    if (redirect){
+        return <Redirect to="/"/>;
+    }
+    return (
+        <form onSubmit={submitEvent}>
+            <h1 className="h3 mb-3 fw-normal">Please sign in</h1>
+            <input type="email" className="form-control" placeholder="Email Address" required
+                   onChange={event => setEmail(event.target.value)}
+            />
+            <input type="password" className="form-control" placeholder="Password" required
+                   onChange={event => setPassword(event.target.value)}
+            />
+            <button className="w-100 btn btn-lg btn-primary" type="submit">Sign in</button>
+        </form>
+    );
+};
+
+export default Login;
